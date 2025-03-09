4eabbce2158ea63c469b56da27c923b785ffad07
smhammadzaidi <smhammadzaidi@gmail.com>
2025-03-09 18:44:36 +0000

    Complete Rock Paper Scissors Game Site

--- app.rb
+++ app.rb
@@ -2,23 +2,20 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
 
 get("/rock") do
 @move = ["rock", "paper", "scissors"]
-@comp_move = move.sample
+@comp_move = @move.sample
 
-if @comp_move == "rock"
-    @outcome = "tied"
-elsif @comp_move == "paper"
-    @outcome = "lost"
-else @comp_move == "scissors"
-    @outcome = "won"
-end
+    if @comp_move == "rock"
+        @outcome = "tied"
+    elsif @comp_move == "paper"
+        @outcome = "lost"
+    else @comp_move == "scissors"
+        @outcome = "won"
+    end
 
 erb(:rock)
 
@@ -26,22 +23,23 @@ end
 
 get("/paper") do
   @move = ["rock", "paper", "scissors"]
-  @comp_move = move.sample
+  @comp_move = @move.sample
   
-  if @comp_move == "paper"
-      @outcome = "tied"
-  elsif @comp_move == "scissors"
-      @outcome = "lost"
-  else @comp_move == "rock"
-      @outcome = "won"
-  end
+    if @comp_move == "paper"
+        @outcome = "tied"
+    elsif @comp_move == "scissors"
+        @outcome = "lost"
+    else @comp_move == "rock"
+        @outcome = "won"
+    end
   
  erb(:paper)
+
 end
 
-  get("/scissors") do
+get("/scissors") do
     @move = ["rock", "paper", "scissors"]
-    @comp_move = move.sample
+    @comp_move = @move.sample
     
     if @comp_move == "scissors"
         @outcome = "tied"
@@ -51,6 +49,7 @@ end
         @outcome = "won"
     end
     
-   erb(:scissors)
-    end
+erb(:scissors)
+
+end
     
